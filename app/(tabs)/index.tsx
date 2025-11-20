55c4f764aee96c5b70edb1c3be7f61f0a74fbd7c
Emiel van der Kraan <emiel.van.der.kraan@student.howest.be>
2025-11-20 15:41:13 +0100

    bordermapimage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -250,6 +250,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
+    borderRadius: 10,
+    borderColor: '#0000',
+    borderWidth: 2,
   },
 
   distanceBadge: {
